3a3b3bf80d902e0d530eb0199a7df2a31f439fc7
Vijay Prakash Sidar <vpmsidar@gmail.com>
2020-05-07 22:54:44 +0530

    Variable Implementation

---
+++ src/Variables/Variable.kt
@@ -0,0 +1,14 @@
+package Variables
+
+fun main(){
+    val name = "Vijay"
+    val age  =10
+    var department:String?   // Here the question mark will define the null value for that variable
+    val college = "OPJIT"
+    department = "Computer Science Department"
+
+    println("Name $name")
+    println("Age : $age")
+    println("Department : $department")
+    println("Department : $college")
+}
